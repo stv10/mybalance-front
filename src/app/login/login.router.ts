56d0dd9dc9b0f27cd2eee7ef1c3288f603a6635e
stv10 <santiagoinbox1@gmail.com>
2024-09-06 15:49:16 -0300

    Refactor login component HTML and SCSS files
    - Updated the login component HTML to use an h1 tag for the title instead of h2
    - Added a divider section with "OR" text between the login form and social login buttons
    - Added social login buttons for Google and Facebook with corresponding icons
    - Added a footer section with a link to the registration page
    
    Also, added a new register component
    
    - Created the register component files with an empty HTML and SCSS file

--- src/app/login/login.router.ts
+++ src/app/login/login.router.ts
@@ -1,5 +1,8 @@
 import { Routes } from "@angular/router";
 import { LoginComponent } from "./login.component";
+import { RegisterComponent } from "./register/register.component";
 export const routes: Routes = [];
 
 routes.push({path: 'login', component: LoginComponent});
+routes.push({path: '', redirectTo: 'login', pathMatch: 'full'});
+routes.push({path: 'register', component: RegisterComponent});
