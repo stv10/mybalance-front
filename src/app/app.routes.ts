07ef6de7d221f08d9136c897078aadb6c865abf2
stv10 <santiagoinbox1@gmail.com>
2024-09-06 13:57:21 -0300

    mockup login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
+import { routes as loginRoutes } from "./login/login.router";
 
 export const routes: Routes = [];
+routes.push(...loginRoutes);
---
+++ src/app/login/login.router.ts
@@ -0,0 +1,5 @@
+import { Routes } from "@angular/router";
+import { LoginComponent } from "./login.component";
+export const routes: Routes = [];
+
+routes.push({path: 'login', component: LoginComponent});
